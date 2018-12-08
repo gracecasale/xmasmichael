c1bce4a98af48841cd8ddda8d0c5102486809495
Grace Casale <gracecasale@gmail.com>
2018-12-07 19:32:15 -0500

    Updates

---
+++ precache-manifest.7aff6a4950c972cd7763e2e9278d8595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f489b183149e01d433db",
+    "url": "/xmasmichael/static/js/runtime~main.f489b183.js"
+  },
+  {
+    "revision": "ffed6a6a9b9337524687",
+    "url": "/xmasmichael/static/js/main.ffed6a6a.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/xmasmichael/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "ffed6a6a9b9337524687",
+    "url": "/xmasmichael/static/css/main.1ed5ce66.chunk.css"
+  },
+  {
+    "revision": "bc0be0f8d2750fabbc8ac7c6ad3effd4",
+    "url": "/xmasmichael/index.html"
+  }
+];
